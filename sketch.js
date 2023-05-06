6c982f2f05f87cfe40b8346e91cac3d045564e69
bngng <132759203+bngng@users.noreply.github.com>
2023-05-06 23:01:09 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -388,12 +388,12 @@ function mousePressed() {
 
   if (d1 < 100) {
     location.reload();
-    location.href = "samesamebutdifferent-11.netlify.app";
+    location.href = "https://samesamebutdifferent-11.netlify.app";
   }
 
   if (d2 < 100) {
     location.reload();
-    location.href = "samesamebutdifferent-01.netlify.app";
+    location.href = "https://samesamebutdifferent-01.netlify.app";
   }
 
   if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < width / 15) {
